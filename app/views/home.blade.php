25b96d3c2af0949d199f0643088955a339e33aac
Joel Kinman <joel.kinman@gmail.com>
2013-06-19 22:14:14 -0700

    stub layouts

---
+++ app/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>Kapital Entertainmant</h1>
+@stop
---
+++ app/views/layout.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <h1>layout</h1>
+
+        @yield('content')
+
+    </body>
+</html>
