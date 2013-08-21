6b3cde5fe953b463bf2b41fb2b82ac90704350fe
Joel Kinman <joel.kinman@gmail.com>
2013-08-20 23:07:17 -0700

    stub for users index

---
+++ app/views/user/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layout')
+
+@section('content')
+
+<!-- Portfolio row of columns -->
+      <div class="row" id="portfolio">
+        <div class="span12">
+          <h1>Portfolio</h1>
+        </div>
+
+      <div class="span3 portthumb">
+        <a href="#" rel="popover" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title"><img src="img/portfolioph.jpg" alt=""></a>
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+
+      <!-- Thumbnails row 2 -->
+
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>  
+      </div>
+
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+      
+      <div class="span3 portthumb">
+        <img src="img/portfolioph.jpg" alt="">
+        <h5>Thumbnail label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+      </div>
+
+@stop
