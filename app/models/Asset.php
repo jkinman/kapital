cde00572e072c8792ee07be37e84f9e244296891
Joel Kinman <joel.kinman@gmail.com>
2013-10-07 00:34:26 -0700

    asset validation rules

--- app/models/Asset.php
+++ app/models/Asset.php
@@ -3,7 +3,12 @@
 class Asset extends Eloquent {
     protected $guarded = array();
 
-    public static $rules = array();
+    public static $rules = array(
+    	'title' => 'required',
+    	'summary' => 'required',
+    	'url' => 'NULL',
+    	'type' => 'NULL'
+    );
 
     public function artist() {
     	return $this->belongsTo( 'Artist' );
