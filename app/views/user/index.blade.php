5ff85b4f032ed529ac97ef6d55cfd96b93443a34
Joel Kinman <joel.kinman@gmail.com>
2013-10-10 23:37:22 -0700

    incremental work on user index

--- app/views/user/index.blade.php
+++ app/views/user/index.blade.php
@@ -1,61 +1,40 @@
 @extends('layout')
 
 @section('content')
-
+{{ print_r( $users )}}
 <!-- Portfolio row of columns -->
-      <div class="row" id="portfolio">
-        <div class="span12">
-          <h1>Portfolio</h1>
-        </div>
+	<div class="row" id="portfolio">
+		<div class="span12">
+			<h1>Portfolio</h1>
+		</div>
 
-      <div class="span3 portthumb">
-        <a href="#" rel="popover" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title"><img src="img/portfolioph.jpg" alt=""></a>
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
+	@foreach ($users as $user)
+		<div class="span3 portthumb">
+			<a href="#" rel="popover" data-content="And here's some amazing content. It's very engaging. right?" data-original-title="A Title">
+				<img src="img/portfolioph.jpg" alt="">
+			</a>
+			<h5>{{{ $user['name'] }}}</h5>
+			<p> {{{ $user['bio'] }}} </p>
+		</div>
+	@endforeach
 
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
+	<div class="span3 portthumb">
+		<img src="img/portfolioph.jpg" alt="">
+		<h5>Thumbnail label</h5>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+	</div>
 
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
+	<div class="span3 portthumb">
+		<img src="img/portfolioph.jpg" alt="">
+		<h5>Thumbnail label</h5>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+	</div>
 
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
+	<div class="span3 portthumb">
+		<img src="img/portfolioph.jpg" alt="">
+		<h5>Thumbnail label</h5>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
+	</div>
 
-      <!-- Thumbnails row 2 -->
-
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>  
-      </div>
-
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
-
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
-      
-      <div class="span3 portthumb">
-        <img src="img/portfolioph.jpg" alt="">
-        <h5>Thumbnail label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum vulputate odio, et sodales augue commodo nec. Nunc interdum dolor.</p>
-      </div>
 
 @stop
