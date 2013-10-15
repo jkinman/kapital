8db784b1b5d5db6f8a7b506c66b2e7c30cc74961
Joel Kinman <joel.kinman@gmail.com>
2013-10-14 21:12:28 -0700

    fix up user view

--- app/views/user/show.blade.php
+++ app/views/user/show.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
 
-{{ print_r( $user )}}
+	<h1>{{{ $user['name'] }}}</h1>
+	<h2>{{{ $artists[ $user['artist_id']] }}}</h2>
+	<h3><a href="$user['facebook_url'] ">Facebook</a></h3>
+	<h3><a href="$user['twitter_url'] ">Twitter</a></h3>
+	<h3>$user['twitter_url'] </h3>
+	<p>$user['summary'] </p>
+
+@stop
