ad14c2e5babc407663808dff655c2199c6fbabfb
Joel Kinman <joel.kinman@gmail.com>
2013-10-16 13:34:06 -0700

    adding click to JS

--- public/js/kapital.js
+++ public/js/kapital.js
@@ -2,3 +2,10 @@
 
 
 filepicker.setKey( 'A8yAqYzKpTycXyMc5hrYCz' );
+
+
+$( 'addPicture' ).on( 'click', function() {
+	filepicker.pickAndStore({},{},function(InkBlobs){
+	   console.log(JSON.stringify(InkBlobs));
+	});
+} );
