ffe51483698c7723e6fbac3af20699f488bbad0e
Joel Kinman <joel.kinman@gmail.com>
2013-10-23 20:05:41 -0700

    adding one image staticly

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -17,13 +17,13 @@
  			  <div class="item">
 				<img src="img/front2.jpg" alt="">
 			  </div>
-			  <div class="item active">
+<!-- 			  <div class="item active">
 				<img src="img/front3.jpg" alt="">
 			  </div>
   			  <div class="item">
 				<img src="img/front1.jpg" alt="">
 			  </div>
- 			</div>
+ --> 			</div>
 			<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
 			<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
 		  </div>
